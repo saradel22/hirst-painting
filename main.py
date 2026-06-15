e0796f82377cf625e02368bf13f26cf66427e2dc
saradel22 <georgesharonsara3@gmail.com>
2026-06-15 22:33:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import turtle
+import random
+turtle.colormode(255)
+t = turtle.Turtle()
+t.speed(0)
+t.penup()
+t.hideturtle()
+
+color_list = [(198, 13, 32), (40, 76, 188), (244, 247, 253), (39, 216, 69), (238, 227, 5), (227, 159, 49), (29, 40, 154), (212, 76, 15), (17, 153, 17), (241, 36, 161), (195, 16, 12), (223, 21, 120), (68, 10, 31), (61, 15, 8), (223, 141, 206), (11, 97, 62), (219, 159, 11), (54, 209, 229), (19, 21, 49), (238, 157, 216), (79, 74, 212), (10, 228, 238), (73, 212, 168), (93, 233, 198), (65, 231, 239), (217, 88, 51), (6, 68, 42), (176, 176, 233), (239, 168, 161), (249, 8, 48), (5, 246, 222), (15, 76, 110), (243, 15, 14), (38, 43, 221)]
+
+t.setheading(225)
+t.forward(300)
+t.setheading(0)
+no_of_dots = 101
+
+for dot_count in range(1, no_of_dots):
+    t.dot(20, random.choice(color_list))
+    t.forward(50)
+
+    if dot_count % 10 == 0:
+        t.setheading(90)
+        t.forward(50)
+        t.setheading(180)
+        t.forward(500)
+        t.setheading(0)
+
+screen = turtle.Screen()
+screen.exitonclick()
